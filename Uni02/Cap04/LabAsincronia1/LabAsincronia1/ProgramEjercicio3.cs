2cbce8a57f07d67f1dee269ccec131177c5d1606
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-06-07 20:28:51 -0300

    Terminado Uni 02 Cap 04 Lab 01 Ej 03

---
+++ Uni02/Cap04/LabAsincronia1/LabAsincronia1/ProgramEjercicio3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LabAsincronia1
+{
+    internal class ProgramEjercicio3
+    {
+        static async Task Main(string[] args)
+        {
+            await ProbarManejoExcepcionesAsync();
+        }
+
+        private static async Task OperacionConErrorAsync()
+        {
+            int tiempo = 2000; // 2 segundos
+            await Task.Delay(tiempo);
+            throw new InvalidOperationException("Error simulado en operación asincrónica");
+        }
+
+        private static async Task ProbarManejoExcepcionesAsync()
+        {
+            try
+            {
+                await OperacionConErrorAsync();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Excepción capturada: " + ex.Message);
+            }
+        }
+    }
+}
