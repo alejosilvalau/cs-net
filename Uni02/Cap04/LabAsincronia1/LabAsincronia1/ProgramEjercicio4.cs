2aa1021889cf30248e12ff95960125279cf68c40
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-06-07 20:41:13 -0300

    Terminado Uni02, Cap 04, Lab 01, Ej 04

---
+++ Uni02/Cap04/LabAsincronia1/LabAsincronia1/ProgramEjercicio4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LabAsincronia1
+{
+    internal class ProgramEjercicio4
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando operación larga con progreso...");
+            IProgress<int> progreso = new Progress<int>(porcentaje =>
+            {
+                Console.WriteLine($"Progreso: {porcentaje}%");
+            });
+
+            await OperacionLargaConProgresoAsync(progreso);
+        }
+
+        private static async Task OperacionLargaConProgresoAsync(IProgress<int> progreso)
+        {
+            int totalPasos = 10;
+            for (int i = 1; i <= totalPasos; i++)
+            {
+                int tiempo = 500; // 0.5 segundo
+                await Task.Delay(tiempo);
+                int porcentaje = (i * 100) / totalPasos; // Calcula el porcentaje de avance
+                progreso.Report(porcentaje); // Reporta el progreso
+            }
+
+        }
+    }
+}
