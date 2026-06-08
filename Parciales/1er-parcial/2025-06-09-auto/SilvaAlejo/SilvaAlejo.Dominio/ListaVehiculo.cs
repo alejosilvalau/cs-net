97e0f0adb8ffe3b5188a9fcbc7291156eaad51ce
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-06-08 11:25:10 -0300

    Último repaso de 1er Parcial

--- Parciales/1er-parcial/2025-06-09-auto/SilvaAlejo/SilvaAlejo.Dominio/ListaVehiculo.cs
+++ Parciales/1er-parcial/2025-06-09-auto/SilvaAlejo/SilvaAlejo.Dominio/ListaVehiculo.cs
@@ -8,12 +8,16 @@ namespace SilvaAlejo.Dominio
         public static Vehiculo? BuscaPatenteLinq(IEnumerable<Vehiculo> vehiculos, string patente)
         {
             return vehiculos.Where(v => v.Patente == patente).FirstOrDefault();
+            //return (from v in vehiculos
+            //        where v.Patente == patente
+            //        select v).FirstOrDefault();
+
         }
 
-        public static List<Vehiculo> BuscaPatentesLinq(IEnumerable<Vehiculo> vehiculos, string patente)
-        {
-            return vehiculos.Where(v => v.Patente == patente && v.Patente.StartsWith("ABC")).ToList();
-        }
+        //public static List<Vehiculo> BuscaPatentesLinq(IEnumerable<Vehiculo> vehiculos, string patente)
+        //{
+        //    return vehiculos.Where(v => v.Patente == patente && v.Patente.StartsWith("ABC")).ToList();
+        //}
 
         public static Vehiculo? BuscaPatenteIterativa(IEnumerable<Vehiculo> vehiculos, string patente)
         {
