bed0808a18d060e228dd0d87a9f6fad4b6b17fbb
Alejo Lautaro Silva <alejo.lautaro@hotmail.com>
2026-06-29 20:25:54 -0300

    Terminado Uni 06 Cap 01 Lab 3 Consumir Escritorio

--- Uni06/Cap01/LabServicios/Lab3ConsumirEscritorio/Form1.Designer.cs
+++ Uni06/Cap01/LabServicios/Lab3ConsumirEscritorio/Form1.Designer.cs
@@ -2,20 +2,8 @@
 {
     partial class Form1
     {
-        private readonly HttpClient _httpClient = new()
-        {
-            BaseAddress = new Uri("http://localhost:5000")
-        };
-
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -27,18 +15,231 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dgvAlumnos = new DataGridView();
+            lblApellido = new Label();
+            lblNombre = new Label();
+            lblLegajo = new Label();
+            lblDireccion = new Label();
+            lblId = new Label();
+            txtApellido = new TextBox();
+            txtNombre = new TextBox();
+            txtLegajo = new TextBox();
+            txtDireccion = new TextBox();
+            txtId = new TextBox();
+            btnAgregar = new Button();
+            btnModificar = new Button();
+            btnEliminar = new Button();
+            btnConsultar = new Button();
+            btnRefrescar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvAlumnos).BeginInit();
+            SuspendLayout();
+            // 
+            // dgvAlumnos
+            // 
+            dgvAlumnos.AllowUserToAddRows = false;
+            dgvAlumnos.AllowUserToDeleteRows = false;
+            dgvAlumnos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvAlumnos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAlumnos.Location = new Point(14, 16);
+            dgvAlumnos.Margin = new Padding(3, 4, 3, 4);
+            dgvAlumnos.MultiSelect = false;
+            dgvAlumnos.Name = "dgvAlumnos";
+            dgvAlumnos.ReadOnly = true;
+            dgvAlumnos.RowHeadersWidth = 51;
+            dgvAlumnos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvAlumnos.Size = new Size(869, 333);
+            dgvAlumnos.TabIndex = 0;
+            dgvAlumnos.SelectionChanged += dgvAlumnos_SelectionChanged;
+            // 
+            // lblApellido
+            // 
+            lblApellido.AutoSize = true;
+            lblApellido.Location = new Point(14, 413);
+            lblApellido.Name = "lblApellido";
+            lblApellido.Size = new Size(69, 20);
+            lblApellido.TabIndex = 3;
+            lblApellido.Text = "Apellido:";
+            // 
+            // lblNombre
+            // 
+            lblNombre.AutoSize = true;
+            lblNombre.Location = new Point(14, 453);
+            lblNombre.Name = "lblNombre";
+            lblNombre.Size = new Size(67, 20);
+            lblNombre.TabIndex = 5;
+            lblNombre.Text = "Nombre:";
+            // 
+            // lblLegajo
+            // 
+            lblLegajo.AutoSize = true;
+            lblLegajo.Location = new Point(14, 493);
+            lblLegajo.Name = "lblLegajo";
+            lblLegajo.Size = new Size(57, 20);
+            lblLegajo.TabIndex = 7;
+            lblLegajo.Text = "Legajo:";
+            // 
+            // lblDireccion
+            // 
+            lblDireccion.AutoSize = true;
+            lblDireccion.Location = new Point(14, 533);
+            lblDireccion.Name = "lblDireccion";
+            lblDireccion.Size = new Size(75, 20);
+            lblDireccion.TabIndex = 9;
+            lblDireccion.Text = "Dirección:";
+            // 
+            // lblId
+            // 
+            lblId.AutoSize = true;
+            lblId.Location = new Point(14, 373);
+            lblId.Name = "lblId";
+            lblId.Size = new Size(25, 20);
+            lblId.TabIndex = 1;
+            lblId.Text = "Id:";
+            // 
+            // txtApellido
+            // 
+            txtApellido.Location = new Point(91, 409);
+            txtApellido.Margin = new Padding(3, 4, 3, 4);
+            txtApellido.Name = "txtApellido";
+            txtApellido.Size = new Size(228, 27);
+            txtApellido.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(91, 449);
+            txtNombre.Margin = new Padding(3, 4, 3, 4);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(228, 27);
+            txtNombre.TabIndex = 6;
+            // 
+            // txtLegajo
+            // 
+            txtLegajo.Location = new Point(91, 489);
+            txtLegajo.Margin = new Padding(3, 4, 3, 4);
+            txtLegajo.Name = "txtLegajo";
+            txtLegajo.Size = new Size(228, 27);
+            txtLegajo.TabIndex = 8;
+            // 
+            // txtDireccion
+            // 
+            txtDireccion.Location = new Point(91, 529);
+            txtDireccion.Margin = new Padding(3, 4, 3, 4);
+            txtDireccion.Name = "txtDireccion";
+            txtDireccion.Size = new Size(228, 27);
+            txtDireccion.TabIndex = 10;
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(91, 369);
+            txtId.Margin = new Padding(3, 4, 3, 4);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(114, 27);
+            txtId.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(366, 369);
+            btnAgregar.Margin = new Padding(3, 4, 3, 4);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(114, 40);
+            btnAgregar.TabIndex = 11;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
+            // 
+            // btnModificar
+            // 
+            btnModificar.Location = new Point(366, 423);
+            btnModificar.Margin = new Padding(3, 4, 3, 4);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(114, 40);
+            btnModificar.TabIndex = 12;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(366, 476);
+            btnEliminar.Margin = new Padding(3, 4, 3, 4);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(114, 40);
+            btnEliminar.TabIndex = 13;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Location = new Point(503, 369);
+            btnConsultar.Margin = new Padding(3, 4, 3, 4);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(114, 40);
+            btnConsultar.TabIndex = 14;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
+            // 
+            // btnRefrescar
+            // 
+            btnRefrescar.Location = new Point(503, 423);
+            btnRefrescar.Margin = new Padding(3, 4, 3, 4);
+            btnRefrescar.Name = "btnRefrescar";
+            btnRefrescar.Size = new Size(114, 40);
+            btnRefrescar.TabIndex = 15;
+            btnRefrescar.Text = "Refrescar";
+            btnRefrescar.UseVisualStyleBackColor = true;
+            btnRefrescar.Click += btnRefrescar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(896, 575);
+            Controls.Add(dgvAlumnos);
+            Controls.Add(lblId);
+            Controls.Add(txtId);
+            Controls.Add(lblApellido);
+            Controls.Add(txtApellido);
+            Controls.Add(lblNombre);
+            Controls.Add(txtNombre);
+            Controls.Add(lblLegajo);
+            Controls.Add(txtLegajo);
+            Controls.Add(lblDireccion);
+            Controls.Add(txtDireccion);
+            Controls.Add(btnAgregar);
+            Controls.Add(btnModificar);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnConsultar);
+            Controls.Add(btnRefrescar);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "ABMC Alumnos";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvAlumnos).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dgvAlumnos;
+        private Label lblApellido;
+        private Label lblNombre;
+        private Label lblLegajo;
+        private Label lblDireccion;
+        private Label lblId;
+        private TextBox txtApellido;
+        private TextBox txtNombre;
+        private TextBox txtLegajo;
+        private TextBox txtDireccion;
+        private TextBox txtId;
+        private Button btnAgregar;
+        private Button btnModificar;
+        private Button btnEliminar;
+        private Button btnConsultar;
+        private Button btnRefrescar;
     }
 }
--- Uni06/Cap01/LabServicios/Lab3ConsumirEscritorio/Form1.cs
+++ Uni06/Cap01/LabServicios/Lab3ConsumirEscritorio/Form1.cs
@@ -1,10 +1,257 @@
+using Dominio;
+using System.Net.Http.Json;
+using System.Text.Json;
+
 namespace Lab3ConsumirEscritorio
 {
     public partial class Form1 : Form
     {
+        private readonly HttpClient _httpClient = new()
+        {
+            BaseAddress = new Uri("http://localhost:5012")
+        };
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            await CargarAlumnos();
+        }
+
+        private List<Alumno> ParseAlumnos(JsonElement[] elementos)
+        {
+            List<Alumno> alumnos = new();
+            foreach (var el in elementos)
+            {
+                Alumno a = new(
+                    el.GetProperty("apellido").GetString()!,
+                    el.GetProperty("nombre").GetString()!,
+                    el.GetProperty("legajo").GetInt32(),
+                    el.GetProperty("direccion").GetString()!
+                );
+                a.setId(el.GetProperty("id").GetInt32());
+                alumnos.Add(a);
+            }
+            return alumnos;
+        }
+
+        private async Task CargarAlumnos()
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync("alumnos");
+                string json = await response.Content.ReadAsStringAsync();
+                JsonElement[] elementos = JsonSerializer.Deserialize<JsonElement[]>(json)!;
+                dgvAlumnos.DataSource = ParseAlumnos(elementos);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al cargar alumnos: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            txtId.Clear();
+            txtApellido.Clear();
+            txtNombre.Clear();
+            txtLegajo.Clear();
+            txtDireccion.Clear();
+            txtId.Focus();
+        }
+
+        private async void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtApellido.Text) || string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtLegajo.Text) || string.IsNullOrWhiteSpace(txtDireccion.Text))
+                {
+                    MessageBox.Show("Complete todos los campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                var alumno = new
+                {
+                    Apellido = txtApellido.Text,
+                    Nombre = txtNombre.Text,
+                    Legajo = int.Parse(txtLegajo.Text),
+                    Direccion = txtDireccion.Text
+                };
+
+                HttpResponseMessage response = await _httpClient.PostAsJsonAsync("alumnos", alumno);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Alumno agregado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LimpiarCampos();
+                    await CargarAlumnos();
+                }
+                else
+                {
+                    MessageBox.Show("Error al agregar alumno.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El legajo debe ser un número.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnModificar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtId.Text))
+                {
+                    MessageBox.Show("Ingrese el ID del alumno a modificar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtApellido.Text) || string.IsNullOrWhiteSpace(txtNombre.Text) ||
+                    string.IsNullOrWhiteSpace(txtLegajo.Text) || string.IsNullOrWhiteSpace(txtDireccion.Text))
+                {
+                    MessageBox.Show("Complete todos los campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int id = int.Parse(txtId.Text);
+                Alumno alumno = new Alumno(txtApellido.Text, txtNombre.Text, int.Parse(txtLegajo.Text), txtDireccion.Text);
+                alumno.setId(id);
+
+                HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"alumnos/{id}", alumno);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Alumno modificado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LimpiarCampos();
+                    await CargarAlumnos();
+                }
+                else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    MessageBox.Show("Alumno no encontrado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Error al modificar alumno.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El ID y el legajo deben ser números.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtId.Text))
+                {
+                    MessageBox.Show("Ingrese el ID del alumno a eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int id = int.Parse(txtId.Text);
+
+                DialogResult result = MessageBox.Show($"¿Está seguro de eliminar el alumno con ID {id}?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    HttpResponseMessage response = await _httpClient.DeleteAsync($"alumnos/{id}");
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        MessageBox.Show("Alumno eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        LimpiarCampos();
+                        await CargarAlumnos();
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        MessageBox.Show("Alumno no encontrado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error al eliminar alumno.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El ID debe ser un número.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtId.Text))
+                {
+                    MessageBox.Show("Ingrese el ID del alumno a consultar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int id = int.Parse(txtId.Text);
+                HttpResponseMessage response = await _httpClient.GetAsync($"alumnos/{id}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    JsonElement el = JsonSerializer.Deserialize<JsonElement>(json);
+
+                    txtId.Text = el.GetProperty("id").GetInt32().ToString();
+                    txtApellido.Text = el.GetProperty("apellido").GetString();
+                    txtNombre.Text = el.GetProperty("nombre").GetString();
+                    txtLegajo.Text = el.GetProperty("legajo").GetInt32().ToString();
+                    txtDireccion.Text = el.GetProperty("direccion").GetString();
+                }
+                else
+                {
+                    MessageBox.Show("Alumno no encontrado.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("El ID debe ser un número.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void btnRefrescar_Click(object sender, EventArgs e)
+        {
+            LimpiarCampos();
+            await CargarAlumnos();
+        }
+
+        private void dgvAlumnos_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvAlumnos.CurrentRow != null)
+            {
+                txtId.Text = dgvAlumnos.CurrentRow.Cells["Id"].Value?.ToString();
+                txtApellido.Text = dgvAlumnos.CurrentRow.Cells["Apellido"].Value?.ToString();
+                txtNombre.Text = dgvAlumnos.CurrentRow.Cells["Nombre"].Value?.ToString();
+                txtLegajo.Text = dgvAlumnos.CurrentRow.Cells["Legajo"].Value?.ToString();
+                txtDireccion.Text = dgvAlumnos.CurrentRow.Cells["Direccion"].Value?.ToString();
+            }
         }
     }
 }
